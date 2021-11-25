e5f1e490b33d0f204db693605a9cffda40e28979
TlahuicoleSystem <barrales.josue.1i@gmail.com>
2021-11-24 21:34:01 -0600

    prueba

---
+++ WPFSOAPClient/Begin/WebServiceClient/WebServiceClient/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WebServiceClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e) 
+        {
+            double dato1 = Convert.ToDouble(txtInput1.Text);
+            double dato2 = Convert.ToDouble(txtInput2.Text);
+            ServiceReference1.MyWebServiceSoapClient client = new ServiceReference1.MyWebServiceSoapClient();
+            double result = client.sumar(dato1, dato2);
+            txtOutput.Text = "" + result;
+        }
+        private void restaCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            double dato1 = Convert.ToDouble(txtInput1.Text);
+            double dato2 = Convert.ToDouble(txtInput2.Text);
+            ServiceReference1.MyWebServiceSoapClient client = new ServiceReference1.MyWebServiceSoapClient();
+            double result = client.restar(dato1, dato2);
+            txtOutput.Text = "" + result;
+        }
+        private void mulCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            double dato1 = Convert.ToDouble(txtInput1.Text);
+            double dato2 = Convert.ToDouble(txtInput2.Text);
+            ServiceReference1.MyWebServiceSoapClient client = new ServiceReference1.MyWebServiceSoapClient();
+            double result = client.mul(dato1, dato2);
+            txtOutput.Text = "" + result;
+        }
+
+        private void divCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            double dato1 = Convert.ToDouble(txtInput1.Text);
+            double dato2 = Convert.ToDouble(txtInput2.Text);
+            ServiceReference1.MyWebServiceSoapClient client = new ServiceReference1.MyWebServiceSoapClient();
+            double result = client.div(dato1, dato2);
+            txtOutput.Text = "" + result;
+        }
+        private void POWCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            double dato1 = Convert.ToDouble(txtInput1.Text);
+            double dato2 = Convert.ToDouble(txtInput2.Text);
+            ServiceReference1.MyWebServiceSoapClient client = new ServiceReference1.MyWebServiceSoapClient();
+            double result = client.potencia(dato1, dato2);
+            txtOutput.Text = "" + result;
+        }
+        private void SQRT_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtInput2.Text!=""){
+                txtInput2.Text = "";
+            }
+            
+                double value1 = Convert.ToDouble(txtInput1.Text);
+                ServiceReference1.MyWebServiceSoapClient client = new ServiceReference1.MyWebServiceSoapClient();
+                double result = client.raiz(value1);
+                txtOutput.Text = "" + result;
+            
+            
+        }
+    }
+}
---
+++ WebServiceApplication/Solution/WebApplication/WebApplication/MyWebService.asmx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebApplication
+{
+    /// <summary>
+    /// Summary description for MyWebService
+    /// </summary>
+    [WebService(Namespace = "http://jomich.com/example/webservice")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class MyWebService : System.Web.Services.WebService
+    {
+
+      
+
+        [WebMethod]
+        public double sumar(double value1, double value2)
+        {
+            return value1 + value2;
+        }
+
+        [WebMethod]
+        public double restar(double value1, double value2)
+        {
+            return value1 - value2;
+        }
+
+        [WebMethod]
+        public double mul(double value1, double value2)
+        {
+            return value1 * value2;
+        }
+
+        [WebMethod]
+        public double div(double value1, double value2)
+        {
+            return value1 / value2;
+        }
+
+        [WebMethod]
+        public double potencia(double value1, double value2)
+        {
+            double res = Math.Pow(value1, value2);
+            return res;
+        }
+
+        [WebMethod]
+        public double raiz(double value1)
+        {
+            double res = Math.Sqrt(value1);
+            return res;
+        }
+    }
+}
